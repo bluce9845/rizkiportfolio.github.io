122620bd6de52a649e5e0cc192166a1b66f9bd6a
bluce9845 <rizkyfiananda@gmail.com>
2023-12-29 11:35:55 +0700

    penambahan alert dan tombol loading

--- script/form.js
+++ script/form.js
@@ -1,9 +1,23 @@
-const scriptURL = "<SCRIPT URL>";
-const form = document.forms["submit-to-google-sheet"];
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzywS-CHOeiwVC1Ex6T5vSfMnoSP7YdemiX-LNKESO73fvdAk4qwB9MiX68CK2rka6wnw/exec";
+const form = document.forms["RIZKI-port-contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // ketika tombol submit di klik
+  // tampilkan button loading, hilangkan button submit
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {
+      // tampilkan button submit, hilangkan button Loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      console.log("Success!", response);
+    })
     .catch((error) => console.error("Error!", error.message));
 });
